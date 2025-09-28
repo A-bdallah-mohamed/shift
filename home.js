47e24119ebc010e4c31f20977d34adfb13dfa6f6
A-bdallah-mohamed <abddallahmohammed21@gmail.com>
2025-09-28 10:58:05 +0300

    join us

--- home.js
+++ home.js
@@ -4,8 +4,19 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+
+const Job_swiper = new Swiper(".job_swiper", {
+  loop: true,
+   slidesPerView: 4,  
+  spaceBetween: 20
 });
